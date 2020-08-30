980ab1df5c4f0fa20cb23c03d17fbe2a9d63bf82
Ed <edtan@users.noreply.github.com>
2020-08-29 23:41:17 -0400

    Add app config template and make app require configuration

--- app-code/main.py
+++ app-code/main.py
@@ -4,5 +4,5 @@ import os
 
 with open('version', 'r') as f:
     version = f.read()
-print(f"This is version {version} of the app!!")
+print(f"This is version {version} of the app using config: {os.getenv('color')}!!")
 print(f"$foobar = {os.getenv('foobar')}")
